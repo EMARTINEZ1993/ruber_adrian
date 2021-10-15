9e94f120d0b6561716b085caaccbd7bba5ea2062
rubergomez <gomescapera@gmail.com>
2021-10-14 21:04:26 -0500

    se sube el inicio

---
+++ inicio.php
@@ -0,0 +1,46 @@
+<?php 
+    $title ="Dashboard - "; 
+    include "head.php";
+    include "sidebar.php";
+
+;
+?>
+    <div class="right_col" role="main"> <!-- page content -->
+        <div class="">
+            <div class="page-title">
+                <div class="row top_tiles">
+                    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
+                        <div class="tile-stats">
+                          <div class="icon"><i class="fa fa-ticket text-info
+                            "></i></div>
+                          <div class="count">333</div>
+                          <h3>Modulo 1</h3>
+                        </div>
+                    </div>
+                    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="tile-stats">
+                          <div class="icon"><i class="fa fa-list-alt text-info"></i></div>
+                          <div class="count">44</div>
+                          <h3>Modulo</h3>
+                        </div>
+                    </div>
+                    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="tile-stats">
+                          <div class="icon"><i class="fa fa-th-list text-info"></i></div>
+                          <div class="count">55</div>
+                          <h3>Modulo</h3>
+                        </div>
+                    </div>
+                    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="tile-stats">
+                          <div class="icon"><i class="fa fa-users text-info"></i></div>
+                          <div class="count">66</div>
+                          <h3>Modulo</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include "footer.php" ?>
